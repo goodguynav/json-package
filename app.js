f784333ea95cc3b292f59e0f547630ab098cb51f
Vaaaaan <yoshi.vyc@gmail.com>
2016-07-28 18:55:56 +0800

    json packages

---
+++ app.js
@@ -0,0 +1,9 @@
+var readlineSync = require('readline-sync');
+
+var userName = readlineSync.question('May I have your name? ');
+console.log('Hi ' + userName + '!');
+ 
+var favFood = readlineSync.question('What is your favorite food? ', {
+  hideEchoBack: true // The typed text on screen is hidden by `*` (default). 
+});
+console.log('Oh, ' + userName + ' loves ' + favFood + '!');
